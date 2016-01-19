b5546d74c7b0029fc48c7e80254b582c1d1fef49
Andre Venancio <noreply@andrevenancio.com>
2016-01-19 18:57:41 +1100

    adding 5th experiment

--- source/experiments/js/5.js
+++ source/experiments/js/5.js
@@ -1,36 +1,50 @@
-var renderer, camera, scene;
+var renderer, camera, scene, controls, clock;
+var data, vertices, geometry, material, mesh, ambient, light;
 
 init();
 update();
 
 function init() {
 
-    scene = new THREE.Scene();
-    scene.fog = new THREE.Fog(0x000000, 0, 10);
+    clock = new THREE.Clock()
 
-    camera = new THREE.PerspectiveCamera(40, window.innerWidth / window.innerHeight, 1, 10000);
+    scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0x000000, 500, 5000);
+
+    camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 10000);
+    camera.position.x = 0;
+    camera.position.y = 500;
     camera.position.z = 1000;
+    camera.lookAt(scene.position);
+    scene.add(camera);
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.setPixelRatio(window.devicePixelRatio || 1);
-    renderer.autoClear = false;
+
+    document.body.appendChild(renderer.domElement);
+
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.minDistance = 1000
+    controls.maxDistance = 2000
+    controls.enablePan = true
+    controls.enableKeys = true
+    controls.minPolarAngle = 10*Math.PI/180
+    controls.maxPolarAngle = 80*Math.PI/180
 
     window.addEventListener('resize', resize.bind(this));
 
-    // window.theme = 'light';
+    addElements();
+
+    window.theme = 'light';
     window.info = [
         '<strong>Date</strong>',
-        '13.01.2016',
+        '19.01.2016',
         '',
         '<strong>Info</strong>',
-        'perlin noise',
+        'Flat Ocean is a study on Three.js ShaderMaterial. It includes fog, lights and uses standard derivatives to calculate per face lighting (kind of).',
         '',
         '<strong>Technology</strong>',
         'WebGL, <a href="http://threejs.org/" target="_blank">Three.js</a></strong>',
-        '',
-        '<strong>Download</strong>',
-        '<a href="/experiments/shaders/4.frag" target="_blank">4.frag</a>',
     ].join('<br>');
 
     resize();
@@ -38,15 +52,46 @@ function init() {
 
 }
 
+function addElements() {
+    geometry = new THREE.PlaneGeometry(10000, 10000, 45, 45);
+    geometry.applyMatrix(new THREE.Matrix4().makeRotationX(-Math.PI / 2));
+
+    material = new THREE.ShaderMaterial({
+       derivatives: FlatShading.derivatives,
+       uniforms: FlatShading.uniforms,
+       fragmentShader: FlatShading.fragment,
+       vertexShader: FlatShading.vertex,
+       lights: true,
+       fog: true
+    });
+    material.uniforms['speed'].value.x = Math.random();
+    material.uniforms['speed'].value.z = 0.5;
+    material.uniforms['terrain_seed'].value = 0.001;
+    material.uniforms['terrain_height'].value = 200;
+
+    mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+    ambient = new THREE.AmbientLight(0x000000);
+    scene.add(ambient);
+
+    light = new THREE.PointLight(0xffffff);
+    light.position.set(0, 2000, 0);
+    scene.add(light);
+}
+
 function update() {
 
     requestAnimationFrame(update);
+    renderer.render(scene, camera);
 
-    renderer.render();
+    mesh.material.uniforms['time'].value = clock.getElapsedTime();
 
 }
 
 function resize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
     renderer.setSize(window.innerWidth, window.innerHeight);
 
